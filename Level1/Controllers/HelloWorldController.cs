2e1de149deb8dac0d2bead32389f40c81107797b
prudhvisreekanth <prudhviramesh1908@gmail.com>
2020-04-13 18:39:14 +0530

    first commit

---
+++ Level1/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult SayHello()
+        {
+            //implement code here
+            return View();
+        }
+    }
+}
